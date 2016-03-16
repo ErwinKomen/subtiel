3397ad66d8ffc431729ad9285fc18756aa746b0b
ErwinKomen <E.Komen@ru.nl>
2016-03-16 14:47:59 +0100

    Synchronization problems for opsubeng cause faulty folia files...

---
+++ opsubcombi/cmbMain.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using opsub;
+
+namespace opsubcombi {
+  /* -------------------------------------------------------------------------------------
+   * Name:  cmbMain
+   * Goal:  entry point of command-line "open subtitle Combi" program
+   *        Harvest, combine, distribute FoLiA and cmdi files
+   * History:
+   * 14/mar/2016 ERK Created
+     ------------------------------------------------------------------------------------- */
+  class cmbMain {
+    // =================== My own static variables =======================================
+    static ErrHandle errHandle = new ErrHandle();
+    // =================== Local variables ===============================================
+    static List<String> lstFolia = null; // List of .folia.xml.gz files in the [sFolia] directory
+    static List<String> lstCmdi = null;  // List of .cmdi.xml files in the [sCmdi] directory
+    // Command-line entry point + argument handling
+    static void Main(string[] args) {
+      String sLanguage = "";    // Which language to take (two-letter code 'en' or 'nl')
+      String sFolia = "";       // Base directory to take (e.g. /vol/tensusers/ekomen)
+      String sSubtiel = "";     // Base directory for the output (e.g: /vol/tensusers/ekomen/subtiel)
+      String sCmdi = "";        // Base directory for CMDI files
+      bool bIsDebug = false;    // Debugging
+
+      try {
+        // Check command-line options
+        for (int i = 0; i < args.Length; i++) {
+          // get this argument
+          String sArg = args[i];
+          if (sArg.StartsWith("-")) {
+            // Check out the arguments
+            switch (sArg.Substring(1)) {
+              case "f": // Root directory under which the .folia.xml.gz files are located
+                sFolia = Path.GetFullPath(args[++i]);
+                break;
+              case "c": // Root directory where the CMDI files are located
+                sCmdi = Path.GetFullPath(args[++i]);
+                break;
+              case "o": // Root directory where the output files are going to be stored
+                sSubtiel = Path.GetFullPath(args[++i]);
+                break;
+              case "d": // Debugging
+                bIsDebug = true;
+                break;
+              case "l": // Language (three letter code)
+                sLanguage = args[++i];
+                break;
+            }
+          } else {
+            // Throw syntax error and leave
+            SyntaxError("1 - i=" + i + " args=" + args.Length + " argCurrent=[" + sArg + "]"); return;
+          }
+        }
+        // Check presence of input/output
+        if (sFolia == "" || !Directory.Exists(sFolia)) { SyntaxError("No (valid) base directory for FoLiA input"); return; }
+        if (sCmdi == "" || !Directory.Exists(sCmdi)) { SyntaxError("No (valid) cmdi directory"); return; }
+        if (sSubtiel == "") { SyntaxError("No subtiel directory"); return; }
+        // If the target directory is not there, create it
+        if (!Directory.Exists(sSubtiel)) {
+          Directory.CreateDirectory(sSubtiel);
+        }
+
+        // Initialise the Treebank Xpath functions, which may make use of tb:matches()
+        XPathFunctions.conTb.AddNamespace("tb", XPathFunctions.TREEBANK_EXTENSIONS);
+
+        // Create a new instance of the combination class
+        cmbConv oConv = new cmbConv(errHandle);
+        oConv.output = sSubtiel;
+
+        // find .cmdi.xml files
+        errHandle.Status("Finding .cmdi.xml files...");
+        lstCmdi = Directory.GetFiles(sCmdi, "*.cmdi.xml", SearchOption.AllDirectories).ToList();
+
+        // Convert the list of files into a dictionary
+        oConv.cmdi(lstCmdi);
+
+        // Find .folia.xml.gz files
+        errHandle.Status("Finding directories in " + sFolia);
+        // Walk all directories
+        errHandle.Status("Processing .folia.xml.gz files...");
+        // Resursive call for each subdirectory.
+        WalkDirectoryTree(sFolia, "*.folia.xml.gz", ref oConv);
+
+        // Save the harvested information to an xml file
+        if (!oConv.harvestSaveXml( sSubtiel + "/harvest.xml")) { 
+          errHandle.DoError("Main", "Could not create harvest summary xml file");
+          return;
+        }
+
+        // Exit the program
+        errHandle.Status("Ready");
+      } catch (Exception ex) {
+        errHandle.DoError("Main", ex); // Provide standard error message
+        throw;
+      }
+    }
+
+    static void WalkDirectoryTree(String sStartDir, String sFilter, ref cmbConv objConv) {
+      String[] arFiles = null;
+      String[] arSubDirs = null;
+
+      // First, process all the files directly under this folder
+      try {
+        arFiles = Directory.GetFiles(sStartDir, sFilter);
+      }
+      // This is thrown if even one of the files requires permissions greater
+      // than the application provides.
+      catch (UnauthorizedAccessException e) {
+        // Only give warning
+        errHandle.Status(e.Message);
+      } catch (System.IO.DirectoryNotFoundException e) {
+        errHandle.Status(e.Message);
+      }
+
+      // Check if all is valid
+      if (arFiles != null) {
+        // bSkip = true;
+        // Walk all files in this directory
+        foreach (String sFile in arFiles) {
+          // Process this file
+          if (!objConv.harvestOneFolia(sFile)) {
+            errHandle.DoError("Main", "Could not process file " + sFile);
+            return;
+          }
+        }
+
+        // Now find all the subdirectories under this directory.
+        arSubDirs = Directory.GetDirectories(sStartDir);
+        // Walk all directories
+        foreach (String sDirName in arSubDirs) {
+          // Resursive call for each subdirectory.
+          WalkDirectoryTree(sDirName, sFilter, ref objConv);
+        }
+      }
+    }
+
+
+    /* -------------------------------------------------------------------------------------
+     * Name:  SyntaxError
+     * Goal:  Show simple syntax error message to the user
+     * History:
+     * 25/jan/2016 ERK Created
+       ------------------------------------------------------------------------------------- */
+    static void SyntaxError(String sChk) {
+      Console.WriteLine(sChk);
+      Console.WriteLine("Syntax: opsubcombi -f foliaDir -c cmdiDir -o outputDir -l language [-d] \n" +
+        "\n\n\tNote: output directory must differ from input one\n");
+    }
+
+
+
+  }
+}
---
+++ opsubeng/oseMain.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using opsub;
+
+namespace opsubeng {
+  /* -------------------------------------------------------------------------------------
+   * Name:  oseMain
+   * Goal:  Entry point of command-line "open subtitle English" program
+   *         This retrieves and processes the English source of a subtitle instance
+   * History:
+   * 1/feb/2016 ERK Created
+     ------------------------------------------------------------------------------------- */
+  class oseMain {
+    // =================== My own static variables =======================================
+    static ErrHandle errHandle = new ErrHandle();
+    // =================== Local variables ===============================================
+    // Command-line entry point + argument handling
+    static void Main(string[] args) {
+      String sDutch = "";         // Input directory for DUTCH subtitles
+      String sEnglish = "";       // Input directory for ENGLISH subtitles
+      String sOutput = "";        // Output directory
+      String sParallel = "";      // File in .xml format that contains the parallel between EN-NL
+      bool bIsDebug = false;      // Debugging
+      bool bForce = false;        // Force
+      String sAction = "english"; // Type of action to be taken
+
+      try {
+        // Check command-line options
+        for (int i = 0; i < args.Length; i++) {
+          // get this argument
+          String sArg = args[i];
+          if (sArg.StartsWith("-")) {
+            // Check out the arguments
+            switch (sArg.Substring(1)) {
+              case "p": // Xml file containing the parallels between Eng and NL
+                sParallel = args[++i];
+                break;
+              case "n": // Input directory for Dutch: .folia.xml and .cmdi.xml files
+                sDutch = args[++i];
+                break;
+              case "e": // Input directory for English: .folia.xml and .cmdi.xml files
+                sEnglish = args[++i];
+                break;
+              case "o": // Top output directory
+                sOutput = args[++i];
+                break;
+              case "f": // Force
+                bForce = true;
+                break;
+              case "a": // Action
+                sAction = args[++i].ToLower();
+                if (sAction != "english") {
+                  SyntaxError("The only action right now is 'english'");
+                  return;
+                }
+                break;
+              case "d": // Debugging
+                bIsDebug = true;
+                break;
+              default:
+                // Throw syntax error and leave
+                SyntaxError("Unknown option: [" + sArg+ "]");
+                return;
+            }
+          } else {
+            // Throw syntax error and leave
+            SyntaxError("1 - i=" + i + " args=" + args.Length + " argCurrent=[" + sArg + "]"); return;
+          }
+        }
+        // Check presence of input/output
+        if (sDutch == "" || sEnglish == "") { SyntaxError("Both dutch and english input must be specified"); return; }
+        if (sParallel == "") { SyntaxError("The XML file containing the Dutch-English parallels must be specified"); return; }
+
+        // Check input directory and parallels file
+        if (!Directory.Exists(sDutch)) { errHandle.DoError("Main", "Cannot find Dutch input file(s) in: " + sDutch); return; }
+        if (!Directory.Exists(sEnglish)) { errHandle.DoError("Main", "Cannot find English input file(s) in: " + sEnglish); return; }
+        if (!File.Exists(sParallel)) { errHandle.DoError("Main", "Cannot find parallel file in: " + sParallel); return; }
+
+        // Initialize the main entry point for the conversion
+        engConv objConv = new engConv(errHandle);
+
+        // Set directories where input is situated and output should come
+        objConv.dutch = sDutch;
+        objConv.english = sEnglish;
+        objConv.output = sOutput;
+
+        // Initialise the Treebank Xpath functions, which may make use of tb:matches()
+        opsub.util.XPathFunctions.conTb.AddNamespace("tb", opsub.util.XPathFunctions.TREEBANK_EXTENSIONS);
+
+        // Start reading the parallels XML
+        ParReader oParallel = new ParReader(sParallel, errHandle);
+        // Walk all the <linkGrp> elements
+        XmlReader rdLinkGrp = null;
+        while (oParallel.getNextLinkGrp(ref rdLinkGrp)) {
+          // Process this one
+          if (!objConv.ConvertOneEngToFolia(rdLinkGrp, bForce, bIsDebug)) {
+            errHandle.DoError("Main", "Could not convert English");
+            return; }
+        }
+        /*
+        XmlNode ndxLinkGrp = oParallel.getNextLinkGrp();
+        while (ndxLinkGrp!= null) {
+          // Process this one
+          if (!objConv.ConvertOneEngToFolia(ndxLinkGrp, bForce, bIsDebug)) { errHandle.DoError("Main", "Could not convert English"); return; }
+          // Go to the next one
+          ndxLinkGrp = oParallel.getNextLinkGrp();
+        }
+        */
+
+        // Exit the program
+        errHandle.Status("Ready");
+      } catch (Exception ex) {
+        errHandle.DoError("Main", ex); // Provide standard error message
+        throw;
+      }
+    }
+
+ 
+    /* -------------------------------------------------------------------------------------
+     * Name:  SyntaxError
+     * Goal:  Show simple syntax error message to the user
+     * History:
+     * 25/jan/2016 ERK Created
+       ------------------------------------------------------------------------------------- */
+    static void SyntaxError(String sChk) {
+      Console.WriteLine("Syntax: opsubeng -n NLinputDir -e ENinputDir -p ParallelXML -o outputDir [-d] [-f] \n" +
+        "\n\n\tNote: output directory must differ from input one\n"+
+        "Message: " + sChk);
+    }
+
+  }
+}
